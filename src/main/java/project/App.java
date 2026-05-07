ab038250f5b120163471f1b316fa0e80c8c82fe2
VUMINH <minhquangvu201@gmail.com>
2026-05-07 10:13:03 +0700

    begin

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -1,13 +1,8 @@
 package project;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static int add(int a, int b){
+        return a + b;
     }
 }
